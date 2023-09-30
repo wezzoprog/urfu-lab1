beb6f19c8d69f9fcf6626a3f712bc2fdf61ad8b2
wezzoprog <139227216+wezzoprog@users.noreply.github.com>
2023-09-30 22:48:56 +0500

    Исправление ошибки ввода и вывода года рождения
    Исправил ошибку связанную с вводом и выводом года рождения (раньше запрашивался год, сейчас запрашивается возраст и посредством новой функции born_year вычисляется год рождения)

--- zadanie2.py
+++ zadanie2.py
@@ -1,6 +1,12 @@
+import datetime
+year = datetime.datetime.today().year
+
+def born_year(age):
+    return year - age
+
 name = str(input('Введите имя: '))
-born_year = int(input('Введите год рождения: '))
+age = int(input('Введите возраст: '))
 city = str(input('Введите город: '))
 country = str(input('Введите страну: '))
 
-print(f'Уважаемый {name}! \nНа сегодняшний день Вы проживаете в стране {country}, в городе {city}, и вы родились в {born_year}')
+print(f'Уважаемый {name}! \nНа сегодняшний день Вы проживаете в стране {country}, в городе {city}, и вы родились в {born_year(age)}')
