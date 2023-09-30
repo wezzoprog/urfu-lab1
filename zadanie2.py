f34e427a866572fecdbdc1e67c1ec7fabf3960c1
wezzoprog <139227216+wezzoprog@users.noreply.github.com>
2023-09-30 22:47:03 +0500

    Create zadanie2.py
    Создание файла программы №2 (запрос и вывод данных о пользователе)

---
+++ zadanie2.py
@@ -0,0 +1,6 @@
+name = str(input('Введите имя: '))
+born_year = int(input('Введите год рождения: '))
+city = str(input('Введите город: '))
+country = str(input('Введите страну: '))
+
+print(f'Уважаемый {name}! \nНа сегодняшний день Вы проживаете в стране {country}, в городе {city}, и вы родились в {born_year}')
